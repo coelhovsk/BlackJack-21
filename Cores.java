78ffe9d8088282ca9f2017bff53e1b2a4b09282c
Henrique Coelho <78321582+coelhovsk@users.noreply.github.com>
2023-12-05 10:56:28 -0300

    Delete Cores.java

--- Cores.java
+++
@@ -1,7 +0,0 @@
-public class Cores {
-    public static final String COR_VERMELHA = "\033[0;91m";    // RED
-    public static final String COR_VERDE = "\033[0;92m";  // GREEN
-    public static final String COR_AMARELA = "\033[0;93m"; // YELLOW
-    public static final String RESETAR_COR = "\u001B[0m"; // RESET
-
-}
